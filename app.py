5511d76e81e0377fc71238dc86c9a7bc9e2e5aa6
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-02-18 10:48:21 +0000

    turn debug to False in app.py

--- app.py
+++ app.py
@@ -487,8 +487,7 @@ def delete_review(review_id):
 ###########################################################################
 
 
-# don't forget to turn debug=false before launch
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
